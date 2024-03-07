384f3a83a6682dff928b381482335952c0e247f3
keltoum <keltoumidhssou5wp@gmail.com>
2024-03-07 13:40:43 +0100

    jshj

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,28 +9,28 @@
         <label for="cr-1" style="--hue: 82"></label>
         <div class="ci" style="--z: 4">
             {{-- <h2 class="ch" style="--h: 32; --s: 80%; --l: 90%">What do you know?</h2> --}}
-            <img src="https://www.tajnid.ma/assets/images/Tajnid_Metier_F.jpg" alt="Snow on leafs">
+            <img src="2.png" alt="Snow on leafs">
         </div>
 
         <input type="radio" name="a" id="cr-2">
         <label for="cr-2" style="--hue: 82"></label>
         <div class="ci" style="--z: 3">
             {{-- <h2 class="ch" style="--h: 82; --s: 80%; --l: 90%">Look from inside?</h2> --}}
-            <img src="https://www.tajnid.ma/assets/images/Tajnid_Metier_H.jpg" alt="Trees">
+            <img src="3.png" alt="Trees">
         </div>
 
         <input type="radio" name="a" id="cr-3">
         <label for="cr-3" style="--hue: 82"></label>
         <div class="ci" style="--z: 2">
             {{-- <h2 class="ch" style="--h: 40; --s: 100%; --l: 89%">In the mountains?</h2> --}}
-            <img src="https://www.tajnid.ma/assets/images/Tajnid_Physique_H.jpg" alt="Mountains and houses">
+            <img src="4.png" alt="Mountains and houses">
         </div>
 
         <input type="radio" name="a" id="cr-4">
         <label for="cr-4" style="--hue: 82"></label>
         <div class="ci" style="--z: 1">
             {{-- <h2 class="ch" style="--h: 210; --s: 70%; --l: 90%">Above looks beautiful?</h2> --}}
-            <img src="https://www.tajnid.ma/assets/images/Tajnid_Formation_H.jpg" alt="Sky and mountains">
+            <img src="5.png" alt="Sky and mountains">
         </div>
 
     </div>
